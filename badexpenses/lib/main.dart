d368123fc5431a1876dd0982b9437a08a9a556ab
rafaelbardsouza <152352367+rafaelbardsouza@users.noreply.github.com>
2024-08-27 17:36:31 -0300

    [feat] layout

--- badexpenses/lib/main.dart
+++ badexpenses/lib/main.dart
@@ -10,12 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bad Expenses',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Bad Expenses Home Page'),
+      home: const MyHomePage(title: '0,00'),
     );
   }
 }
@@ -34,12 +29,54 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.black,
+        title: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                widget.title,
+                style: TextStyle(
+                  color: Colors.greenAccent[700],
+                  fontSize: 80.0,
+                  fontWeight: FontWeight.w200,
+                ),
+              ),
+              SizedBox(height: 20.0),
+              const Text(
+                'that could be yours',
+                style: TextStyle(
+                  color: Color.fromARGB(200, 255, 255, 255),
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
+        toolbarHeight: 250.0,
       ),
-      body: const Center(
-        child: Text('Bad Expenses'),
+      body: Container(
+        color: Color.fromARGB(255, 26, 26, 26),
+        child: const Center(
+          child: Column(
+            children: [
+            ],
+          ),
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        },
+        backgroundColor: Color.fromARGB(36, 255, 255, 255),
+        child: const Text(
+          '💸',
+          style: TextStyle(
+            fontSize: 30.0,
+          ),
+        )
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
